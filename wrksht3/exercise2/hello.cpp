146e6a6d412a7def4b6411ce25554fb54bb9fc50
etanigaki2 <eeyet5@nottingham.ac.uk>
2025-04-27 17:36:25 +0100

    WS3 exercise3 completed.2

---
+++ wrksht3/exercise2/hello.cpp
@@ -0,0 +1,10 @@
+//begin - hello.cpp ---------
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+std::cout<< "hello Planet earth!" << std::endl;
+return 0;
+}
+
+// end - hello.cpp ---------
